0628e6c5548286bb2f6192f54ff471ef38c079ce
aaron <aaron.k.boodoosingh@gmail.com>
2023-05-07 23:18:33 -0400

    fixed some code, still has errors

--- website/__init__.py
+++ website/__init__.py
@@ -1,18 +1,44 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'monkeytoe 12321323'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-
+    db.init_app(app)
+    app.config['UPLOAD_PHOTOS_DEST'] = 'uploads'
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/') #Creating searches
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    photos = UploadSet('photos', IMAGES)
+    configure_uploads(app, photos)
+
+    class UploadForm(FlaskForm):
+        photo = FileField(validators=[
+            FileAllowed(photos, 'Only images are allowed'), 
+            FileRequired('Field should not be empty')
+        ])
+
+        submit = SubmitField('Upload')
+
+    from .models import User, Note
     
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+
     return app
